aeec0f8efbcc51f8ba007494a5c43ccd3a1e9b7a
lakhanrnandaniya3112 <lakhanrnandaniya3112@gmail.com>
2024-12-02 07:10:01 +0000

    code conduct done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 121212
+let accountEmail = "lakhan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+let accountState;
+
+accountEmail = "test@gmail.com"
+accountPassword = "121212"
+accountCity = "Gandhinagar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
