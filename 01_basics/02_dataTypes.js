1df3e8df43e0e81da148f80518418deff3f7f84d
lakhanrnandaniya3112 <lakhanrnandaniya3112@gmail.com>
2024-12-02 09:56:34 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -9,5 +9,7 @@ console.log("Lakhan")
 let name = "Lakhan"
 let age = 26
 let isLoggedIn = false
- 
-console.log(typeof "Lakhan");
+
+console.log(typeof "Lakhan");  // string
+console.log(typeof "undefined"); // undefined
+console.log(typeof "null"); // object
