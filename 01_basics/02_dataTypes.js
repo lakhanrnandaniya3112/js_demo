2ad1695cdc76033c0d8b0c47144a967f3aa6be7d
lakhanrnandaniya3112 <lakhanrnandaniya3112@gmail.com>
2024-12-02 07:35:07 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js new  version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Lakhan")
+
+let name = "Lakhan"
+let age = 26
+let isLoggedIn = false
