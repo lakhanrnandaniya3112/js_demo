7fa705e46881482f49cd7a3d4ebb7b2566c194e4
lakhanrnandaniya3112 <lakhanrnandaniya3112@gmail.com>
2024-12-02 11:02:21 +0000

    conversion Opration done

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 1212
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
